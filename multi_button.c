dec0802e82001a16bced5da42568e135c906653c
Meco Jianting Man <920369182@qq.com>
2021-04-22 09:56:37 +0800

    Update multi_button.c

--- multi_button.c
+++ multi_button.c
@@ -169,7 +169,7 @@ void button_handler(struct button* handle)
                 handle->state = 0;
             }
         }
-        else if(handle->ticks > LONG_TICKS)
+        else if(handle->ticks > SHORT_TICKS)
         {
             handle->state = 0;
         }
